6179d6f80033857c94843a496506205899d1a1fb
jeremytjimenez <jeremytjimenez@gmail.com>
2023-05-12 03:18:10 -0400

    lab complete

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,8 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { useParams } from 'react-router-dom'
+import CatsDogs from './CatsDogs'
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -12,19 +14,33 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  const params = useParams()
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
+        {params.kind === "cats" ? 
+        cats.map((cat) => (
+          <Pet key={cat.id} kind="cat" pet={cat} />
+        )) : null}
+
+        {params.kind === "dogs" ? dogs.map((dog) => (
+          <Pet key={dog.id} kind="dog" pet={dog} />
+        )) : null}
+
+        {params.kind ? null : <CatsDogs dogs={dogs} cats={cats}/>}
+
         {/* All cats section */}
+        {/* {}
         {cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        ))} */}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
+        {/* {dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        ))} */}
       </section>
     </section>
   );
