f2b92fb80cdeafc2a784e3a8aed9b55fbebed7b4
Akira Watson-Brown <akirawatson-brown@Akiras-Air.home>
2023-05-15 00:08:31 -0400

    Unstable-AB

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { Link } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -18,12 +19,17 @@ export const PetsList = ({ pets }) => {
       <section className="pets-list">
         {/* All cats section */}
         {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
+          <Link key={cat.id} to={`/pets/cats/${cat.id}`}>
+            <Pet key={cat.id} kind="cat" pet={cat} />
+          </Link>
         ))}
 
         {/* All dogs section */}
+
         {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
+          <Link key={dog.id} to={`/pets/dogs/${dog.id}`}>
+            <Pet key={dog.id} kind="dog" pet={dog} />
+          </Link>
         ))}
       </section>
     </section>
