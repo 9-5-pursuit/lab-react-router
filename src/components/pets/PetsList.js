14f70554b1fe6490239f92de8e1085c5ddb5f319
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-05-16 01:14:15 -0400

    Completed

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
 export const PetsList = ({ pets }) => {
+  
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -12,19 +15,51 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+
+const { pathname } = useLocation();
+const pathNameArray = pathname.split("/");
+let url = pathNameArray.pop();
+
+let petData = null;
+
+if(url === "cats"){
+  petData = (
+  <section>
+    {cats.map((cat) => (
+      <Pet key={cat.id} kind="cat" pet={cat} />
+    ))}
+  </section>
+  )
+}
+else if(url === "dogs"){
+  petData = (
+  <section>
+    {dogs.map((dog) => (
+      <Pet key={dog.id} kind="dog" pet={dog} />
+    ))}
+  </section>
+  )
+}
+else if(url === "pets"){
+  petData = (
+    <>
+      {cats.map((cat) => (
+      <Pet key={cat.id} kind="cat" pet={cat} />
+      ))}
+
+      {dogs.map((dog) => (
+      <Pet key={dog.id} kind="dog" pet={dog} />
+      ))} 
+    </>
+  )
+}
+
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
-
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {petData}
       </section>
     </section>
   );
