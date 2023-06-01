183c999b2c9150c5e2ef005842714308661e29b2
Devin Lewis <devinlewis@pursuit.org>
2023-06-01 10:03:09 -0400

    Complete

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -2,32 +2,35 @@ import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
-export const PetsList = ({ pets }) => {
-  const [cats, dogs] = pets.reduce(
-    (acc, pet) => {
-      const position = pet.kind === "Cat" ? 0 : 1;
-      acc[position].push(pet);
-      return acc;
-    },
-    [[], []]
-  );
+export const PetsList = ({ pets, kind }) => {
+    const [cats, dogs] = pets.reduce(
+        (acc, pet) => {
+            const position = pet.kind === "Cat" ? 0 : 1;
+            acc[position].push(pet);
+            return acc;
+        },
+        [[], []]
+    );
 
-  return (
-    <section className="pets-wrapper">
-      <PetsListNav cats={cats} dogs={dogs} />
-      <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+    return (
+        <section className="pets-wrapper">
+            <PetsListNav cats={cats} dogs={dogs} />
+            <section className="pets-list">
+                {/* All cats section */}
 
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
-      </section>
-    </section>
-  );
+                {kind === "cats" &&
+                    cats.map((cat) => (
+                        <Pet key={cat.id} kind="cat" pet={cat} />
+                    ))}
+
+                {/* All dogs section */}
+                {kind === "dogs" &&
+                    dogs.map((dog) => (
+                        <Pet key={dog.id} kind="dog" pet={dog} />
+                    ))}
+            </section>
+        </section>
+    );
 };
 
 export default PetsList;
