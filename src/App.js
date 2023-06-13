325160a1982f4b765a73b819f7ed6e1a52426220
daleskiii <aun2793@gmail.com>
2023-06-13 13:00:05 -0400

    sreact-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-/*
-  Components
-*/
 import Nav from "./components/common/Nav";
 import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import StaffList from "./components/staff/StaffList";
 import PetsList from "./components/pets/PetsList";
 
-/*
-  Data
-  ---------------
-  Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
-*/
 import { employeeData } from "./data/employees.js";
 import { ownerData } from "./data/owners";
 import { petData } from "./data/pets";
@@ -25,10 +18,25 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home employees={employees} owners={owners} pets={pets} />}
+          />
+          <Route path="/staff" element={<StaffList employees={employees} />} />
+          {/* <Route path="/pets" element={<PetsList pets={pets} />} />
+          <Route path="/pets/cats" element={<PetsList pets={pets} />} />
+          <Route path="/pets/dogs" element={<PetsList pets={pets} />} /> */}
+
+          <Route path="/pets" element={<PetsList pets={pets} />}>
+            <Route path="dogs" element={<PetsList pets={pets} />} />
+            <Route path="cats" element={<PetsList pets={pets} />} />
+          </Route>
+        </Routes>
+      </Router>
+
       <Footer />
     </div>
   );
